32f89ddb99f5e2aa03644b10a04355bec8ffdc80
deepaksahu3698 <deepaksahu3698@gmail.com>
2022-09-19 22:27:26 +0530

    Add clock

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -42,3 +42,48 @@ var Controls = React.createClass({
 });
 
 module.exports = Controls;
+var React = require('react');
+var Clock = require('Clock');
+
+var Controls = React.createClass({
+
+  propTypes: {
+    countdownStatus: React.PropTypes.string.isRequired,
+    onStatusChange: React.PropTypes.func.isRequired
+  },
+
+  onStatusChange: function (newStatus) {
+    // currying patten function generates a function (in this case passed down from parent)
+    return () =>{
+      this.props.onStatusChange(newStatus);
+    }
+  },
+
+    // componentWillReceiveProps: function(newProps){
+    //   console.log('componentWillRecieveProps', newProps.countdownStatus);
+    // },
+
+  render: function() {
+
+    var {
+      countdownStatus
+    } = this.props;
+
+    var renderStartStopButton = () => {
+      if (countdownStatus === 'started') {
+        return <button className="button secondary" onClick={this.onStatusChange("paused")}>Pause</button>
+      } else {
+        return <button className="button primary" onClick={this.onStatusChange("started")}>Start</button>
+      }
+    };
+
+    return (
+      <div className="controls">
+        {renderStartStopButton()}
+        <button className="button alert hollow" onClick={this.onStatusChange("stopped")}>Clear</button>
+    </div>);
+  }
+});
+
+module.exports = Controls;
+
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -15,3 +15,20 @@ var Main = (props) => {
 }
 
 module.exports = Main;
+var React = require('react');
+var Navigation = require('Navigation');
+
+var Main = (props) => {
+  return (
+    <div>
+      <Navigation/>
+      <div className = "row">
+        <div className="column small-centered medium-6 large-4">
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Main;
